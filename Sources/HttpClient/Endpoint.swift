4bacb10f09646c41e7a197bcd8ae4b14915e5d0f
Christophe Bronner <christophe.bronner@videotron.ca>
2024-10-09 06:10:36 -0400

    Update Endpoint requirements

--- Sources/HttpClient/Endpoint.swift
+++ Sources/HttpClient/Endpoint.swift
@@ -11,5 +11,27 @@ public protocol Endpoint<Response>: Decodable {
 	associatedtype Response: Encodable
 
 	var method: HttpMethod { get }
-	var url: URL { get }
+	var path: String { get }
+	var query: [URLQueryItem]? { get }
+}
+
+public extension Endpoint {
+
+	@inlinable
+	var query: [URLQueryItem]? { nil }
+
+	@inlinable
+	func url(from baseURL: URL) -> URL {
+		if let query {
+			var components = URLComponents()
+			components.path = path
+			components.queryItems = query
+			return components.url(relativeTo: baseURL)!
+		}
+		if #available(macOS 13.0, *) {
+			return URL(filePath: path, relativeTo: baseURL)
+		} else {
+			return URL(fileURLWithPath: path, relativeTo: baseURL)
+		}
+	}
 }
